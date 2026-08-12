1956ae2473e564a116937872e7cbaa1570e6442e
norahe0304-art <norahe0304@gmail.com>
2026-08-12 01:39:34 -0400

    Update Global Resiliency naming and demo QA

---
+++ assets/HomePage-CNXXvcgn.js
@@ -0,0 +1,1 @@
+import{j as s,ak as a}from"./index-DRJkD6Tw.js";const o=()=>s.jsx("section",{className:"py-6 px-8 font-mona",children:s.jsx(a,{})});export{o as default};
--- assets/HomePage-DR03OQic.js
+++
@@ -1,1 +0,0 @@
-import{j as s,ak as a}from"./index-BvZ3EZaj.js";const o=()=>s.jsx("section",{className:"py-6 px-8 font-mona",children:s.jsx(a,{})});export{o as default};
